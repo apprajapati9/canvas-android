c6fa061d05eb62e0e1639a4e1522981d0cb1cec9
Ajay P. Prajapati <apprajapati@hotmail.com>
2024-08-30 20:25:25 -0400

    statuscolor and navigationbarcolor change

--- app/src/main/java/com/apprajapati/solarsystem/MainActivity.kt
+++ app/src/main/java/com/apprajapati/solarsystem/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.apprajapati.solarsystem
 
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,13 +12,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var solarView: SolarView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //Setting navbar and status bar color from here.
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.dark(ContextCompat.getColor(this, R.color.black)),
+            statusBarStyle = SystemBarStyle.dark(ContextCompat.getColor(this, R.color.black))
+        ) // This determined status bar and navigation bar color, investigate.
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //window.navigationBarColor = ContextCompat.getColor(this, R.color.black) // This works too but edgeToEdge() is the way to go.
         solarView = findViewById(R.id.solarView)
     }
 
